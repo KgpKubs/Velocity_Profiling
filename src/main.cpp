0e27600b1d4e398b41da22b1d6407e7ff88c24a2
vernwalrahul <vernwalrahul@gmail.com>
2017-11-30 20:24:12 +0530

    updated

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,16 @@
 using namespace std;
 
 const int MAX_VEL = 50, BOT_ID = 0;
-ros::Publisher pub;
+ros::Publisher pub, pub_to_gui;
+krssg_ssl_msgs::planner_path path_points_pub;
+krssg_ssl_msgs::point_2d point_;
 krssg_ssl_msgs::gr_Commands final_msgs;
 krssg_ssl_msgs::gr_Robot_Command command_msgs;
 
 void send_stop()
 {
 	cout<<"sending stop\n";
-	command_msgs.veltangent  = 0;
+	command_msgs.veltangent  = 0 ;
 	command_msgs.velnormal   = 0;
 	final_msgs.isteamyellow   = false;
 	final_msgs.robot_commands = command_msgs;
@@ -21,10 +23,11 @@ void send_stop()
 
 void send_vel(double speed, double motion_angle)
 {
+	speed/=1000 ;
 	cout<<"motion_angle = "<<motion_angle<<endl;
 	double bot_angle = home_pos_theta[BOT_ID];
 	double vel_tangent = speed*cos(bot_angle - motion_angle);
-	double vel_normal = speed*sin(bot_angle - motion_angle);
+	double vel_normal = -speed*sin(bot_angle - motion_angle);
 
 	command_msgs.veltangent  = vel_tangent;
 	command_msgs.velnormal   = vel_normal;
@@ -36,6 +39,7 @@ void send_vel(double speed, double motion_angle)
 	cout<<"Path_point[0] = "<<path_points[0].x<<","<<path_points[0].y<<endl;
 	cout<<"Path_point[10] = "<<path_points[10].x<<","<<path_points[10].y<<endl;
 	cout<<"vel_angle[0] = "<<vel_angle[0]<<endl;
+	cout<<"N,T = "<<vel_normal<<","<<vel_tangent<<endl;
 	return;
 }
 
@@ -47,6 +51,15 @@ void Callback_BS(const krssg_ssl_msgs::BeliefState::ConstPtr& msg)
 		return;
 	}
 
+	path_points_pub.point_array.clear();
+	for(int i=0;i<path_points.size();i++)
+	{
+		  point_.x=path_points[i].x;
+	      point_.y=path_points[i].y;
+	      path_points_pub.point_array.push_back(point_);
+	}
+    pub_to_gui.publish(path_points_pub);
+
 	for(int i=0;i<6;i++)
 	{
 		home_pos_theta[i] = msg->homePos[i].theta;
@@ -55,6 +68,13 @@ void Callback_BS(const krssg_ssl_msgs::BeliefState::ConstPtr& msg)
 	}
 	curr_time = ros::Time::now().toSec();
 	double t = curr_time - start_time;
+
+	if(t>ExpectedTraverseTime)
+	{
+		cout<<"OUT OF TIME!! ExpectedTraverseTime = "<<ExpectedTraverseTime<<"\n";
+		send_stop();
+		return;
+	}
 	// cout<<"start_time = "<<start_time<<endl;
 	// cout<<"Calling Trapezoid!! t = "<<t<<endl;
 	if(trapezoid(t, distance_traversed, out_speed))
@@ -66,7 +86,7 @@ void Callback_BS(const krssg_ssl_msgs::BeliefState::ConstPtr& msg)
 			send_stop();
 			return;
 		}
-		send_vel(out_speed, PI/2.0);
+		send_vel(out_speed, vel_angle[index]);
 		// cout<<"Error = "<<dist(path_points[index], home_pos[BOT_ID])<<endl;
 		// cout<<"Travelled "<<distance_traversed/path_length*100<<"percent \n";
 		// cout<<"Going with vel = "<<out_speed<<" at "<<vel_angle[index]*180/(PI)<<" degree\n";
@@ -81,9 +101,9 @@ void Callback_BS(const krssg_ssl_msgs::BeliefState::ConstPtr& msg)
 int flag = 0;
 void Callback(const krssg_ssl_msgs::planner_path::ConstPtr& msg)
 {
-	// if(flag==1) return;
-	// flag=1;
-	// cout<<"in path_planner Callback function. \n";
+	if(flag==1) return ;
+	flag=1;
+	cout<<"in path_planner Callback function. \n";
 	PATH_RECEIVED = true;
 	path_points.clear();
 	vel_angle.clear();
@@ -99,8 +119,8 @@ void Callback(const krssg_ssl_msgs::planner_path::ConstPtr& msg)
 
 		if(i<size_-4 && i>1)
 		{
-			double dx = path_points[i+2].x - path_points[i-2].x;
-			double dy = path_points[i+2].y - path_points[i-2].y;
+			double dx1 = path_points[i+2].x - path_points[i-2].x;
+			double dy1 = path_points[i+2].y - path_points[i-2].y;
 
 			vel_angle.push_back(atan2(dy, dx));
 		}
@@ -119,15 +139,15 @@ void Callback(const krssg_ssl_msgs::planner_path::ConstPtr& msg)
 		}
 	}
 
-	start_point.x = msg->point_array[0].x;
+	start_point.x = msg->point_array[0].x ;
 	start_point.y = msg->point_array[0].y;
 	goal_point.x = msg->point_array[size_-1].x;
 	goal_point.y = msg->point_array[size_-1].y;
 
 	path_length = GetPathLength();
 
-	ExpectedTraverseTime = getTime(0, path_length, MAX_SPEED, MAX_ACC, START_SPEED, FINAL_SPEED);
-
+	ExpectedTraverseTime = getTime(path_length, path_length, MAX_SPEED, MAX_ACC, START_SPEED, FINAL_SPEED);
+	cout<<"ExpectedTraverseTime = "<<ExpectedTraverseTime<<"\n";
 	start_time = ros::Time::now().toSec();
 	curr_time =  ros::Time::now().toSec();
 	// cout<<"setting start_time = "<<start_time<<endl;
@@ -140,6 +160,7 @@ int main(int argc, char **argv)
   	ros::NodeHandle n;
 	ros::Subscriber sub = n.subscribe("/path_planner_ompl", 1000, Callback);
 	pub = n.advertise<krssg_ssl_msgs::gr_Commands>("/grsim_data", 1000);
+	pub_to_gui = n.advertise<krssg_ssl_msgs::planner_path>("/vel_profile_to_gui", 1000);
 	ros::Subscriber sub1 = n.subscribe("/belief_state", 1000, Callback_BS);
 	ros::spin();
 	return 0;
